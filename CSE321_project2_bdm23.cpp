258f0364027a08a7450a8e4aa9a52131996bed0b
gytanzo <gytanzo@gmail.com>
2020-10-09 13:12:10 -0400

    Forgot the header file. Added the PDF as a source reference.

---
+++ CSE321_project2_bdm23.cpp
@@ -0,0 +1,30 @@
+/*
+File: CSE321_project2_bdm23.cpp
+
+Author1: Ben Miller (bdm23@buffalo.edu)
+Date: Fall 2020
+Partner: I worked alone
+Course: CSE321
+
+Summary of File:
+    
+    Purpose: This code is the implementation of a security system lock. The lock should lock/unlock based on a 4 digit code (9407).
+    Modules/Subroutines: None presently. Will update if this changes.
+    Assignment: Corresponds to Project 2.
+    Input(s): A matrix keypad.
+    Output(s): An LED; an LCD display. 
+    Constraint(s): Must run indefinitely, must have a response if an incorrect password is given, must "lock" and "unlock"
+    Source references: Project PDF file, included in this folder
+    
+*/
+
+
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+    }
+}
